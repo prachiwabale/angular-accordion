94a81edc0267b886cd91327cad5cdb2fccfc6c00
prachiwabale <prachiwabale2091993@gmail.com>
2022-05-24 15:53:17 +0530

    3 files modified

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -16,9 +16,7 @@ export class AccordionComponent implements OnInit {
   }
 
   mergeConfig(options: Config) {
-    // 기본 옵션
     const config = {
-      // selector: '#accordion',
       multi: true
     };
 
@@ -26,14 +24,13 @@ export class AccordionComponent implements OnInit {
   }
 
   toggle(index: number) {
-    // 멀티 오픈을 허용하지 않으면 타깃 이외의 모든 submenu를 클로즈한다.
+  
     if (!this.config.multi) {
       this.menus
         .filter((menu, i) => i !== index && menu.active)
         .forEach(menu => (menu.active = !menu.active));
     }
 
-    // Menu의 active를 반전
     this.menus[index].active = !this.menus[index].active;
   }
 }
